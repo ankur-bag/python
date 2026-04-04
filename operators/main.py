c79ec11946998ef180a4c2643941c5e73062d33e
Ankur Bag <ankurbag700@gmail.com>
2026-04-04 19:05:35 +0530

    logical operations

--- operators/main.py
+++ operators/main.py
@@ -1,3 +1,40 @@
 a =22
 b =56
 print(a+b)
+
+print(b/a)
+
+print(b//a) #no decimal will be shown
+
+print(5**2) #5 to the power 2 (5^2)
+
+print(32%5)
+
+print(12+4/2-8) # python follows BODMAS
+
+
+#assignment operator
+a=23
+
+#compound assignment operations
+
+a = 20
+a = a+40
+a = a+60 
+
+print(a)
+
+#comparison operators (provide boolean results)
+
+a = 20
+b = 20
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#logical operators
+
