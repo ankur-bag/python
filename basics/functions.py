13e4fc3e8afe58fc82e7fabe532e5da0fccf71ae
Ankur Bag <ankurbag700@gmail.com>
2026-04-04 18:40:56 +0530

    basics

---
+++ basics/functions.py
@@ -0,0 +1,31 @@
+a = 12
+a = str(a)
+print(a)
+
+#type conversion
+
+b = "12"
+c = int(b)
+print(type(c))
+
+
+#type casting
+
+d = 12.5
+e = int(d)
+print(e)
+
+
+#type checking
+
+f = 12
+g = "hello"
+h = 12.5
+i = True
+
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+
+  
---
+++ basics/main.py
@@ -0,0 +1,22 @@
+sher = "Ankur"
+a = -25
+
+v = 44j #complex number
+
+
+print(type(v))
+
+b = 'K'
+print(ord(b)) # character to unicode
+
+g = 65
+print(chr(g)) # unicode to character
+
+c = "ANKUR"
+print(c[1]) #positive indexing
+print(c[-2]) #negative indexing
+
+d = "ANKUR BAG"
+print (d[0:5]) #slicing
+print(d[0:5:2]) #slicing with step
+
