437ad60d3dae6637fda24d96f057122fe92c0991
Ankur Bag <ankurbag700@gmail.com>
2026-04-05 20:41:49 +0530

    functions

---
+++ functions/main.py
@@ -0,0 +1,30 @@
+# Functions
+
+# def hello():
+#     print("this is helllo functions so i am doing hello")
+
+# hello()
+
+
+def sum(a,b):
+    print("the sum of you num is : ",a+b)
+
+sum(10,20)
+
+
+def sub(a,b):
+    print("the sub of you num is : ",a-b)
+
+sub(10,20)
+
+
+def mul(a,b):
+    print("the mul of you num is : ",a*b)
+
+mul(10,20)
+
+
+def div(a,b):
+    print("the div of you num is : ",a/b)
+
+div(10,20)
