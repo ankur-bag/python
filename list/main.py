085a742d494338061ed6befef96c5cb8dc4a9c20
Ankur Bag <ankurbag700@gmail.com>
2026-04-05 23:58:01 +0530

    list

---
+++ list/main.py
@@ -0,0 +1,30 @@
+# List powers
+'''
+- mutable:
+
+s = [1,2,3,4,5]
+s[0] = 10
+print(s)
+
+- duplicates :
+
+s = [1,2,3,4,5,1,2,3,4,5]
+print(s)
+
+- ordered :
+
+s = [1,2,3,4,5]
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+- heterogenous :
+
+s = [1,2,3,4,5,"hello",True,3.14]
+print(s)
+
+'''
+
+
