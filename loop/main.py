74b291a82f5b5bfa7d434ebbe4be095a4093e8c6
Ankur Bag <ankurbag700@gmail.com>
2026-04-05 19:37:53 +0530

    for loop

---
+++ loop/main.py
@@ -0,0 +1,20 @@
+#Foor loop
+
+# for i in range(5,51,5):
+#     # 5 --> 51 with gap/steps of 5
+#     print(i)
+
+
+
+# n = int(input("Which table do you want? "))
+
+# for i in range(n,n*10+1,n):
+#     print(i)
+
+a= "ANKUR BAG"
+print("Length: ",len(a)) #len starts from 1
+for i in range (len(a)):
+    print(a[i]) # indexing starts from 0
+
+for i in a: 
+    print(i)
---
+++ loop/qn.py
@@ -0,0 +1,41 @@
+# n = int(input("please tell your number: "))
+
+# for i in range(n):
+#     print("hello world")
+
+
+# n = int(input("tell your num: "))
+
+# for i in range(n+1):
+#     print(i)
+
+
+#reverse
+# n = int(input("tell your num: "))
+# for i in range(n,0,-1):
+#     print(i)
+
+
+#print table
+# n = int(input("tell your num: "))
+# for i in range (1,11):
+#     print(f"{n} * {i} = {n*i}")
+
+# char =0
+# dig=0
+# spchr =0
+# a= "gsrtgagehe$%@123rytheqqerg"
+
+# for i in a :
+#     if i.isdigit():
+#         dig +=1
+#     elif i.isalpha():
+#         char+=1
+#     else:
+#         spchr+=1
+
+# print("characters: ",char)
+# print("digits: ",dig)
+# print("special characters: ",spchr)
+
+
