8a1918473c9bb1074acb1f1db8d4568b801dc800
Ankur Bag <ankurbag700@gmail.com>
2026-04-06 02:02:07 +0530

    list 2

--- list/main.py
+++ list/main.py
@@ -28,3 +28,28 @@ print(s)
 '''
 
 
+a = [12,13,14,15,16,17,18,19]
+
+
+#1st way - using index
+# for i in range (len(a)):
+#     print(a[i])
+    
+# #2nd way - directly on values
+
+# for i in a:
+#     print(i)
+    
+
+# l = [1,2,3,4,5,6] #append function
+
+# l.append(80)
+# l.append(40)
+
+# print(l)
+
+
+l = [1,2,3,4,5,6]
+l.insert(0,2)
+
+print(l)
