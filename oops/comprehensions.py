22504b58bb50a5fe98ea141f801ce4583dad9bbf
Ankur Bag <ankurbag700@gmail.com>
2026-04-16 12:35:16 +0530

    comprehensions

---
+++ oops/comprehensions.py
@@ -0,0 +1,14 @@
+# COMPREHENSIONS
+
+# l =[]
+# for i in range(1,21):
+#     if i%2 ==0 :
+#        l.append(i)
+
+# print(l)
+        
+# l =[i for i in range(1,21) if i%2 ==0]
+# print(l)
+
+l ={i: i**2 for i in range(1,10)}
+print(l)
